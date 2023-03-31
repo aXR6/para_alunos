73057ee3edfac5a57adf93b92bce67fda4a83876
root <thalles_tsc@hotmail.com>
2023-03-31 10:59:57 -0300

    Script para automatizar a instalação dos principais softwares.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sobre: Script de automação de instalação em bash para instalar o Google Chrome, VMWarePlayer, VSCode e VLC com um menu de escolha para o usuário. <br>
+
+# LEIA
+```
+Para executar o script, basta salvá-lo em um arquivo com a extensão .sh, dar permissão de execução com o comando chmod +x nome_do_arquivo.sh e executá-lo com o comando ./install.sh
+```
+
+<b># Redes Sociais<b><br>
+Linkedin: https://www.linkedin.com/in/thalles-canela/ <br>
+YouTube:  https://www.youtube.com/c/aXR6CyberSecurity <br>
+Facebook: https://www.facebook.com/axr6PenTest <br>
+Github:   https://github.com/ThallesCanela <br>
+Github:   https://github.com/aXR6 <br>
+Twitter:  https://twitter.com/Axr6S <br>
+Padim:    https://www.padrim.com.br/aXR6CyberSecurity <br>
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# função para imprimir o menu
+print_menu() {
+    echo "Selecione o que deseja instalar:"
+    echo "1. Google Chrome"
+    echo "2. VMWarePlayer"
+    echo "3. VSCode"
+    echo "4. VLC"
+    echo "0. Sair"
+}
+
+# loop do menu
+while true; do
+    print_menu
+    read -p "Opção: " opcao
+
+    case $opcao in
+        1)
+            # instalação do Google Chrome
+            wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+            echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+            sudo apt-get update
+            sudo apt-get install google-chrome-stable
+            ;;
+        2)
+            # instalação do VMWarePlayer
+            sudo apt-get update
+            sudo apt-get install -y build-essential linux-headers-$(uname -r)
+            wget https://download3.vmware.com/software/player/file/VMware-Player-16.1.1-17801498.x86_64.bundle
+            sudo sh VMware-Player-16.1.1-17801498.x86_64.bundle
+            ;;
+        3)
+            # instalação do VSCode
+            wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+            sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+            sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+            sudo apt-get install apt-transport-https
+            sudo apt-get update
+            sudo apt-get install code
+            ;;
+        4)
+            # instalação do VLC
+            sudo apt-get update
+            sudo apt-get install -y vlc
+            ;;
+        0)
+            # sair do script
+            exit
+            ;;
+        *)
+            # opção inválida
+            echo "Opção inválida. Tente novamente."
+            ;;
+    esac
+done
