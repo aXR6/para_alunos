19fb34189ade88dfe9a0aec201fe16ec8b80f92c
Thalles Canela <thalles_tsc@hotmail.com>
2023-05-03 20:43:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ print_menu() {
     echo "2. VMWarePlayer"
     echo "3. VSCode"
     echo "4. VLC"
+    echo "5. Docker"
     echo "0. Sair"
 }
 
@@ -43,6 +44,20 @@ while true; do
             # instalação do VLC
             sudo apt-get update
             sudo apt-get install -y vlc
+            ;;
+        5)
+            # instalação do Docker
+            sudo apt-get update
+            sudo apt-get install ca-certificates curl gnupg
+            sudo install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            sudo chmod a+r /etc/apt/keyrings/docker.gpg
+            echo \
+            "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+            "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo apt-get update
+            sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
             ;;
         0)
             # sair do script
